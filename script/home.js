81a648cd0972760a2a1a010e0f7056aae73da353
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-10-17 21:09:41 +0200

    Add side all pages

--- script/home.js
+++ script/home.js
@@ -1,98 +1,87 @@
-const toggleButton = document.getElementById('toggleButton');
-const searchBar = document.getElementById('searchBar');
-const searchButton = document.getElementById('searchButton');
+const toggleButton = document.getElementById("toggleButton");
+const searchBar = document.getElementById("searchBar");
+const searchButton = document.getElementById("searchButton");
 
-toggleButton.addEventListener('click', () => {
-  searchBar.classList.toggle('hidden');
+toggleButton.addEventListener("click", () => {
+  searchBar.classList.toggle("hidden");
 });
 
-searchButton.addEventListener('click', () => {
-  const query = searchBar.querySelector('input').value;
+searchButton.addEventListener("click", () => {
+  const query = searchBar.querySelector("input").value;
   if (query) {
     getImages(query);
   }
 });
 
-const getImages = function(query) {
+const getImages = function (query) {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
     .then((res) => {
       if (res.ok) {
         return res.json();
       } else {
-        throw new Error('Errore nella response');
+        throw new Error("Errore nella response");
       }
     })
     .then((data) => {
       window.location.href = `artist.html?query=${query}`;
     })
     .catch((error) => {
-      console.error('Si è verificato un errore:', error);
+      console.error("Si è verificato un errore:", error);
     });
 };
 
-const btn = document.getElementById('elimina_pubbl');
-btn.addEventListener('click', function() {
-  console.log('Pulsante cliccato');
-  let cardElement = document.getElementById('porcodio')
+const btn = document.getElementById("elimina_pubbl");
+btn.addEventListener("click", function () {
+  console.log("Pulsante cliccato");
+  let cardElement = document.getElementById("porcodio");
   if (cardElement) {
-    cardElement.classList.add('delete');
+    cardElement.classList.add("delete");
   }
 });
 
+//  FUNZIONE PER INSERIRE GLI ALBUM NELLA BARRA LATERALE SINISTRA
 
-const playlistMenu = document.getElementById('playlist')
-
+const playlistMenu = document.getElementById("playlist");
 const getAlbum = function (query) {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${query}`)
-
     .then((res) => {
       if (res.ok) {
         return res.json();
       } else {
-        throw new Error('Errore nella response');
+        throw new Error("Errore nella response");
       }
     })
-    .then((data)=>{
-      const app = document.getElementById('albumlevate')
-      const card = document.createElement('div')
-      card.innerHTML=`<a class="text-decoration-none text-white" href="album.html?query=${data.id}"><div class="d-flex align-items-center mt-2"><img src="${data.cover_medium}" width="30px" height="30px" /><p>${data.title}<p></div><a>`
-      app.appendChild(card)
-      console.log(data)
-      
+    .then((data) => {
+      const app = document.getElementById("albumlevate");
+      const card = document.createElement("div");
+      card.innerHTML = `<a class="text-decoration-none text-white" href="album.html?query=${data.id}"><div class="d-flex align-items-center mt-2"><img src="${data.cover_medium}" width="30px" height="30px" /><p>${data.title}<p></div><a>`;
+      app.appendChild(card);
+      console.log(data);
     })
     .catch((error) => {
-      console.error('Si è verificato un errore:', error);
-    })
-  }
+      console.error("Si è verificato un errore:", error);
+    });
+};
 
-
-
-
-const albumbtn = document.getElementById('azz')
-albumbtn.addEventListener('click',()=>{
-
-  const via =document.getElementById('levate')
-  via.classList.add('d-none')
-  const numeriArrey = [6415260,1362101,12207660,455130,454043,]
-  invocagetalbum(numeriArrey)
-  const fakeVia = document.getElementById('albumlevate')
-  fakeVia.classList.remove('d-none')
-  
-}
-)
-function invocagetalbum(numeriArrey){
-  for (const id of numeriArrey)
-  getAlbum(id)
+// FUNZIONE BOTTONE ALBUM
+const albumbtn = document.getElementById("azz");
+albumbtn.addEventListener("click", () => {
+  const via = document.getElementById("levate");
+  via.classList.add("d-none");
+  const numeriArrey = [6415260, 1362101, 12207660, 455130, 454043];
+  invocagetalbum(numeriArrey);
+  const fakeVia = document.getElementById("albumlevate");
+  fakeVia.classList.remove("d-none");
+});
+function invocagetalbum(numeriArrey) {
+  for (const id of numeriArrey) getAlbum(id);
 }
 
-const playlistbtn = document.getElementById('azz2')
-playlistbtn.addEventListener('click',()=>{
-  const via =document.getElementById('albumlevate')
-  via.classList.add('d-none')
-  const realVia = document.getElementById('levate')
-  realVia.classList.remove('d-none')
-  
-
-}
-)
-
+// FUNZIONE BOTTONE PLAYLIST
+const playlistbtn = document.getElementById("azz2");
+playlistbtn.addEventListener("click", () => {
+  const via = document.getElementById("albumlevate");
+  via.classList.add("d-none");
+  const realVia = document.getElementById("levate");
+  realVia.classList.remove("d-none");
+});
