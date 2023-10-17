62c4a22411ecfe18df33803bb72179a5e3752d88
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-10-17 19:57:19 +0200

    adds username-edit feature

--- script/profile.js
+++ script/profile.js
@@ -1,15 +1,29 @@
 const pencilIcon = document.getElementById("pencil");
 const personIcon = document.getElementById("person");
 const iconsContainer = document.getElementById("icons-container");
+const threeDots = document.getElementsByClassName("bi-three-dots")[0];
+const userName = document.getElementById("username");
+const div = document.getElementById("form-container");
+const form = document.getElementsByTagName("form")[0];
+const main = document.getElementsByTagName("main")[0];
+const footer = document.getElementsByTagName("footer")[0];
+
+// HOVER ICONE - IMMAGINE PROFILO
 personIcon.addEventListener("mouseover", () => {
-  pencilIcon.classList.remove("d-none");
-  personIcon.classList.add("d-none");
+  if (iconsContainer.style.backgroundImage !== "none") {
+    pencilIcon.classList.remove("d-none");
+    personIcon.classList.add("d-none");
+  }
 });
 
 pencilIcon.addEventListener("mouseleave", () => {
-  pencilIcon.classList.add("d-none");
-  personIcon.classList.remove("d-none");
+  if (iconsContainer.style.backgroundImage !== "none") {
+    pencilIcon.classList.add("d-none");
+    personIcon.classList.remove("d-none");
+  }
 });
+
+// FUNZIONE PER INSERIRE L'IMMAGINE PROFILO
 
 const openImageUploader = function openImageUploader() {
   const imageUpload = document.getElementById("image-upload");
@@ -17,7 +31,6 @@ const openImageUploader = function openImageUploader() {
   pencilIcon.click();
 };
 
-//
 let profileImageDataURL;
 
 const displaySelectedImage = function () {
@@ -31,10 +44,47 @@ const displaySelectedImage = function () {
       profileImageDataURL = dataURL; // Store the profile image data URL
       styleContainer = iconsContainer.style;
       styleContainer.backgroundImage = `url('${profileImageDataURL}')`;
-      styleContainer.backgroundSize = "contain";
+      styleContainer.backgroundSize = "cover";
       styleContainer.backgroundRepeat = "no-repeat";
       styleContainer.backgroundPosition = "center center";
     };
     reader.readAsDataURL(selectedImage);
   }
+  if (iconsContainer.style.backgroundImage !== "none") {
+    console.log("The div has a background image.");
+    personIcon.classList.add("d-none");
+    personIcon.classList.remove("bi");
+    personIcon.classList.remove("bi-person");
+    iconsContainer.addEventListener("mouseover", () => {
+      pencilIcon.classList.remove("d-none");
+    });
+    iconsContainer.addEventListener("mouseleave", () => {
+      pencilIcon.classList.add("d-none");
+    });
+  } else {
+    console.log("The div does not have a background image.");
+  }
 };
+
+// FUNZIONE PER CAMBIARE NOME UTENTE
+
+userName.addEventListener("click", () => {
+  div.classList.remove("d-none");
+
+  main.style.opacity = 0.4;
+  footer.style.opacity = 0.4;
+  div.style.opacity = 1;
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userNameInput = document.getElementById("username-input");
+  userName.innerHTML = userNameInput.value;
+  div.classList.add("d-none");
+  main.style.opacity = 1;
+  footer.style.opacity = 1;
+  localStorage.setItem("username", userName.innerHTML);
+});
+
+const usernameStorage = localStorage.getItem("username");
+userName.innerHTML = usernameStorage ? usernameStorage : "Nome utente";
